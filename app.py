d904adc6879d71d88ac33c502d9ba9581e9c7c59
besttecsystem2025-cell <besttecsystem2025@gmail.com>
2026-01-19 11:39:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,3 @@ def chat():
         return response.text
     except Exception as e:
         return f"Erro na IA: {str(e)}"
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
